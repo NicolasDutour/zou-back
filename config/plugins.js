553177179454df128d53b7445416efa1d5863639
nicolas dutour <dutourn99@gmail.com>
2024-01-09 15:21:04 +0100

    feat: add netlify plugin for deploy

--- config/plugins.js
+++ config/plugins.js
@@ -14,4 +14,18 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  "netlify-deployments": {
+    enabled: true,
+    config: {
+      accessToken: env('NETLIFY_DEPLOYMENTS_PLUGIN_ACCESS_TOKEN'),
+      sites: [
+        {
+          name: 'zou-back',
+          id: "c694e6c8-06a3-4e1a-9aba-7e2be9340558",
+          buildHook: "https://api.netlify.com/build_hooks/659d53fee7092f008df24152",
+          branch: 'main'
+        }
+      ]
+    },
+  },
 });
