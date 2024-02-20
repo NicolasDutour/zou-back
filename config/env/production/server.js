a9446bccf44118f3132aa722fe139a52c7c7539e
nicolas dutour <dutourn99@gmail.com>
2024-02-20 19:43:45 +0100

    feat: Allows cookies to be sent over cross-site requests

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -11,4 +11,12 @@ module.exports = ({ env }) => ({
       secret: env('ADMIN_JWT_SECRET'),
     },
   },
+  session: {
+    secret: env('SESSION_SECRET'),
+    cookie: {
+      secure: env.bool('SESSION_SECURE', true), // Set to true if you're using HTTPS
+      maxAge: 86400000, // 24 hours
+      sameSite: 'none', // Allows cookies to be sent over cross-site requests
+    },
+  },
 })
